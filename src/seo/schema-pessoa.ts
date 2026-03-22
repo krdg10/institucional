27113d2b737949aa05d7b8d08b7b46f4806773d7
kelvin.guia <kelvin.guia@azi.com.br>
2026-03-21 22:16:56 -0400

    [FRONT-END] Ajustes de indexação

---
+++ src/seo/schema-pessoa.ts
@@ -0,0 +1,27 @@
+export const schemaPessoa = {
+    "@context": "https://schema.org",
+    "@graph": [
+        {
+            "@type": "Person",
+            name: "Débora Fernandes",
+            jobTitle: "Psicóloga Clínica",
+            url: "https://psicologadeborafernandes.com.br",
+            image: "https://psicologadeborafernandes.com.br/me.jpg",
+            sameAs: ["https://instagram.com/deborafernandes.psi"],
+        },
+        {
+            "@type": "ProfessionalService",
+            name: "Psicóloga Débora Fernandes",
+            description:
+                "Psicóloga clínica com atendimento online para todo o Brasil.",
+            url: "https://psicologadeborafernandes.com.br",
+            areaServed: "BR",
+            address: {
+                "@type": "PostalAddress",
+                addressLocality: "Londrina",
+                addressRegion: "PR",
+                addressCountry: "BR",
+            }
+        }
+    ]
+};

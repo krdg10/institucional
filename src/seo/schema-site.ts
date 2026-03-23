40929250c6b884989c85785adf4f4c3311afbd2b
kelvin.guia <kelvin.guia@azi.com.br>
2026-03-23 18:18:22 -0400

    [FRONT-END] Ajustes de schema

---
+++ src/seo/schema-site.ts
@@ -0,0 +1,7 @@
+export const schemaSite = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "url": "https://psicologadeborafernandes.com.br",
+    "name": "Psicóloga Débora Fernandes",
+    "alternateName": "Débora Fernandes",
+};
